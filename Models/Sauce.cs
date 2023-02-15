27587ef4ee19ad98e69ef5fccc5d8f87fd767a21
Rogelio Mitre <mitrerogelio@outlook.com>
2023-02-15 00:01:06 -0600

    add db

---
+++ Models/Sauce.cs
@@ -0,0 +1,8 @@
+namespace grocery_api.Models;
+
+public class Sauce
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using grocery_api.Data;
+using grocery_api.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add the PizzaContext
+builder.Services.AddSqlite<PizzaContext>("Data Source=grocery-api.db");
+
+// Add the PromotionsContext
+
+builder.Services.AddScoped<IPizzaService, PizzaService>();
 
 var app = builder.Build();
 
@@ -21,5 +31,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Creating database if none exists (CreateDbIfNotExists):
 
 app.Run();
